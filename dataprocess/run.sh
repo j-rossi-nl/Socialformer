bd543cbb62980d8dc35299a7a76b29ab9b51a16e
Julien Rossi <j.rossi@uva.nl>
2022-04-22 12:02:36 +0200

    quote the $ separator

--- dataprocess/run.sh
+++ dataprocess/run.sh
@@ -1,6 +1,6 @@
-python gen_dynamic_centrality_weight.py
+python gen_dynamic_centrality_weight.py --model_path "bert-base-uncased"
 python gen_dynamic_distance_weight.py
 python gen_static_centrality_weight.py
 python gen_static_distance_weight.py
-paste -d $ data/demo.json weights/tfidf.txt weights/bert.txt weights/dynamic.txt > weights/total.txt
+paste -d "$" data/demo.json weights/tfidf.txt weights/bert.txt weights/dynamic.txt > weights/total.txt
 python main.py
